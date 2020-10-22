2a8d3f71a20f5a51e6991eec8d88ec12b4050d5c
Daniel Segovia Arce <segoarce90@gmail.com>
2020-10-22 16:28:56 -0700

    header height init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
+import { Box, Container, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+import copy from './copy.json'
+
+const useStyles = makeStyles({
+  header: {
+    backgroundColor: 'silver'
+  }
+})
 
 function App() {
+  const headerRef = useRef(null)
+  const classes = useStyles()
+  const [height, setHeight] = useState(null)
+
+  const setHeaderHeight = () => {
+    const { height: headerHeight } = headerRef.current.getBoundingClientRect()
+    setHeight(headerHeight)
+  }
+
+  useEffect(() => {
+    setHeaderHeight()
+    window.addEventListener('resize', setHeaderHeight)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box style={{ paddingTop: height }}>
+      <Box
+        py={3}
+        textAlign="center"
+        component="header"
+        position="fixed"
+        width="100%"
+        top="0"
+        left="0"
+        className={classes.header}
+        ref={headerRef}
+      >
+        <Container>
+          <Typography variant="h1">
+            Hello from header
+          </Typography>
+        </Container>
+      </Box>
+
+      <Box py={5}>
+        <Container>
+          {copy.paragraphs.map((item, idx) => (
+            <Box mb={3}>
+              <Typography variant="body1">{item}</Typography>
+            </Box>
+          ))}
+        </Container>
+      </Box>
+    </Box>
   );
 }
 
